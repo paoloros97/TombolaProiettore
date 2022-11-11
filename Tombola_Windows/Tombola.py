13b3dec50798f26a6781c32f7bab00902696965e
Paolo <paolorosettani97@gmail.com>
2022-11-11 11:36:49 +0100

    comments

--- Tombola_Windows/Tombola.py
+++ Tombola_Windows/Tombola.py
@@ -8,31 +8,30 @@ import random
 class Tabellone:
     def __init__(self, master):
 
-        self.coloreEstratto = "#ff0000" # Colorazione del numero sul tabellone estratto
-        self.coloreBase = "#ffffff" # Colorazione base del numero sul tabellone (non estratto)
+        self.coloreEstratto = "#ff0000" # Colorazione del numero estratto sul tabellone 
+        self.coloreBase = "#ffffff" # Colorazione base del numero (non estratto) sul tabellone 
 
-        # Finestra Tabellone
+        # __Finestra Tabellone__
         self.master = master
         self.master.title('Tombola by Ros')
         self.master.bind("<F11>", lambda event: self.master.attributes("-fullscreen", not self.master.attributes("-fullscreen")))
         self.master.bind("<Escape>", lambda event: self.master.attributes("-fullscreen", False)) 
-
-        # Dimensione automatica della finestra
+        
         window_width = self.master.winfo_screenwidth()
         window_height = self.master.winfo_screenheight()
-        self.master.geometry ("%dx%d"%(window_width,window_height))
+        self.master.geometry ("%dx%d"%(window_width,window_height)) # Dimensione automatica della finestra alle dimensioni dello schermo
 
-        self.button = [] # Inizializzazione della griglia di numeri (Buttons)
+        self.button = [] # Inizializzazione della griglia di numeri/buttons
         num = 0 # Contatore che poi sarà utilizzato come indice della lista button
-        for i in range(0, 9):
-            for j in range(0, 10):
+        for i in range(0, 9): # rows
+            for j in range(0, 10): # columns
                 num = num + 1
                 self.button.append(tk.Button(self.master, text=str(num), bg = '#ffffff', font=("", 60), command = lambda c = num-1: self.changeColor(c)))
-                self.button[-1].grid(row=i, column=j, sticky="EWNS")
+                self.button[-1].grid(row=i, column=j, sticky="EWNS") # -1 is the last element in the list
                 tk.Grid.rowconfigure(self.master, i, weight = 1)
                 tk.Grid.columnconfigure(self.master, j, weight = 1)
         
-        # Finestra Estrattore
+        # __Finestra Estrattore__
         self.estrattore = tk.Toplevel(self.master)
         self.estrattore.title("Estrattore")
         self.estrattore.protocol("WM_DELETE_WINDOW", self.disable_event) # Disabilita il pulsante X della finestra
@@ -58,12 +57,12 @@ class Tabellone:
 
     def nuovaGiocataFair(self):
         print('Nuova estrazione.')
-        self.estratti = set() #Re-inizializza il set di deposito numeri estratti
+        self.estratti = set() #Inizializza il set di deposito numeri estratti
         
         for btn_number in range(len(self.button)):
-            self.button[btn_number]["bg"] = self.coloreBase
+            self.button[btn_number]["bg"] = self.coloreBase # Ricolora tutti i buttons al colore base
 
-        self.status_label.config(text = "N° estratto") # Reimposta il visualizzatore nell'estrattore
+        self.status_label.config(text = "N° estratto") # Reimposta il visualizzatore numeri nell'estrattore
 
     def EstraiFair(self):
         attuali = len(self.estratti) # Conta il totale dei numeri estratti fin'ora
@@ -73,12 +72,12 @@ class Tabellone:
             print("Fine")
             return
 
-        numero = random.randrange(90) # Estrae un numero casuale intero tra 0 e 89
+        numero = random.randrange(90) # Estrae un numero casuale intero tra 0 e 89 inclusi
         self.estratti.add(numero) # Aggiunge il numero estratto al Set
 
         if len(self.estratti) == attuali: # Se il numero era già stato estratto prima, si ripete l'estrazione.
             self.EstraiFair() # Chiamata ricorsiva
-        else: # Altrimenti lo si mostra sul tabellone.
+        else: # Altrimenti, se il numero è nuovo, lo si mostra sul tabellone.
             print(numero + 1) # +1 perchè il "numero" rappresenta l'indice
             self.changeColor(numero) # Colora il numero estratto sul tabellone  
             self.status_label.config(text = str(numero + 1)) # Stampa il numero estratto sulla finestra dell'Estrattore
